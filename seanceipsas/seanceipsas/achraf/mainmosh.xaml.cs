b99dca216740f2e897ea74c61558918862a72306
Ben_Alaya_Achraf <Ben_Alaya_Achraf@outlook.com>
2017-12-20 01:39:46 +0100

    part 1 : adding label+ slider +stacklayout

---
+++ seanceipsas/seanceipsas/achraf/mainmosh.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace seanceipsas.achraf
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class mainmosh : ContentPage
+	{
+		public mainmosh ()
+		{
+			InitializeComponent ();
+		    //Content = new Label
+		    //{
+
+      //          HorizontalOptions = LayoutOptions.Center,
+      //          VerticalOptions = LayoutOptions.Center,
+      //          Text = "Hello World"
+		    //};
+		}
+	}
+}
